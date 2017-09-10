f2d15b9b628cd2615eddc9ee3bdf271d5da29b9f
Josh Nelson <josh.nelson1@yahoo.com>
2017-09-09 20:04:30 -0600

    Added a player controller and a basic player and floor.
    The controller code uses unity's input manager and should work for both keyboard and controller easily. My controller didn't register though so I couldnt test. WASD is movement Left and Right arrows are rotation.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed = 5f;
+    public float rotateSpeed = 150f;
+    public float jumpSpeed = 200f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+        //Movement
+        //TODO: could set up a dictionary of inputs or use delegates and callbacks
+        //TODO: Change to using force for movement and rotation?
+   
+   
+        if (Input.GetButton("Horizontal Key")|| Input.GetButton("Horizontal Stick"))
+        {
+            transform.localPosition += Input.GetAxis("Horizontal Stick")*Input.GetAxis("Horizontal Key")*transform.right * Time.deltaTime * moveSpeed;
+        }
+
+        if (Input.GetButton("Vertical Key")|| Input.GetButton("Vertical Stick"))
+        {
+            transform.localPosition += Input.GetAxis("Vertical Stick") * Input.GetAxis("Vertical Key") * transform.forward * Time.deltaTime * moveSpeed;
+        }
+
+        //Rotation    
+        if (Input.GetButton("Rotate Key")|| Input.GetButton("Rotate Stick"))
+        {
+            transform.Rotate(0, Input.GetAxis("Rotate Stick") * Input.GetAxis("Rotate Key")*Time.deltaTime * rotateSpeed, 0);
+        }
+
+
+    }
+}
